eb34ef808421dca943cea05f575be8a7a071e5e2
Vampire <113182606+sarangalakshitha@users.noreply.github.com>
2022-09-15 16:07:54 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['94722603543'] //ur owner number
+global.ownername = "Saranga Lakshitha" //ur owner name
+global.ytname = "YT: Lk Bro" //ur yt chanel name
+global.socialm = "GitHub: Vampire.." //ur github or insta name
+global.location = "Sri Lanka, Rathnapura, Ayagama" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
-global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.owner = ['94722603543']
+global.ownertag = '94722603543' //ur tag number
+global.botname = '𝚅𝙰𝙼𝙿𝙸𝚁𝙴ᴮᴼᵀ' //ur bot name
+global.linkz = "https://www.youtube.com/channel/UCczmuUxHuVN-yM278P_QPdQ" //your theme url which will be displayed on whatsapp
+global.websitex = "https://www.youtube.com/channel/UCczmuUxHuVN-yM278P_QPdQ" //ur website to be displayed
+global.botscript = 'https://github.com/sarangalakshitha' //script link
+global.reactmoji = "🤖" //ur menu react emoji
+global.themeemoji = "❤" //ur theme emoji
+global.packname = "𝚅𝙰𝙼𝙿𝙸𝚁𝙴ᴮᴼᵀ" //ur sticker watermark packname
+global.author = "𝚅𝙰𝙼𝙿𝙸𝚁𝙴ᴮᴼᵀ" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -79,7 +79,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['916909137213'] //ur premium numbers
+global.premium = ['94722603543'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
